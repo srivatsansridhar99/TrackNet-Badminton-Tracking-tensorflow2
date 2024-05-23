899600b81f990c27684b9893b3c3638328e0e65f
Srivatsan Sridhar <srivatsansridhar99@gmail.com>
2024-05-23 17:05:09 +0530

    changing video2img

--- video2img.py
+++ video2img.py
@@ -10,6 +10,8 @@ HEIGHT=args.HEIGHT
 WIDTH=args.WIDTH
 sigma = args.sigma
 mag = args.mag
+data_load_path = args.data_load_path
+out_dir = args.data_save_path
 
 def video2img(video, csv, output_path, match):
     """
@@ -54,7 +56,7 @@ def video2img(video, csv, output_path, match):
         count += 1
 
 if __name__ == '__main__':
-    raw_dir = 'raw_data'
+    raw_dir = data_load_path
     videos = sorted(glob.glob(os.path.join(raw_dir, '*.mp4')))
     csvs = sorted(glob.glob(os.path.join(raw_dir, '*.csv')))
 
@@ -67,7 +69,7 @@ if __name__ == '__main__':
             raise NameError("Video files and csv files are not corresponded")
 
         print("Convert Video: {}".format(video))
-        output_path = 'train_data/match%d'%(match)
+        output_path = f'{out_dir}/match%d'%(match)
         if not os.path.isdir(output_path):
             os.mkdir(output_path)
             os.mkdir(os.sep.join([output_path, 'x_data']))
