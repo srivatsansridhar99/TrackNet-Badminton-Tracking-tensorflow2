96474f44c0fa114fd798cef199a97224ed900133
Srivatsan Sridhar <srivatsansridhar99@gmail.com>
2024-06-10 10:16:28 +0530

    increasing scope of gpu context

--- train.py
+++ train.py
@@ -71,58 +71,59 @@ wait = 0
 best_loss = float('inf')
 losses = []
 for i in range(epochs):
-	x_train, x_test, y_train, y_test = split_train_test(match_list, ratio=train_test_split, shuffle=True)
-	train_steps = check_steps(x_train+x_test, BATCH_SIZE, FRAME_STACK)
-	print("==========Epoch {}, Train steps: {}, Learning rate: {:.4f}==========".format(i, train_steps, float(K.get_value(model.optimizer.lr))))
 	with tf.device('/GPU:0'):
+		x_train, x_test, y_train, y_test = split_train_test(match_list, ratio=train_test_split, shuffle=True)
+		train_steps = check_steps(x_train+x_test, BATCH_SIZE, FRAME_STACK)
+		print("==========Epoch {}, Train steps: {}, Learning rate: {:.4f}==========".format(i, train_steps, float(K.get_value(model.optimizer.lr))))
+
 		data = data_generator(BATCH_SIZE, x_train+x_test, y_train+y_test, FRAME_STACK)
 		history = model.fit(data,
 							steps_per_epoch=train_steps,
 							epochs=1,
 							verbose=1)
-	loss = sum(history.history['loss'])
-	losses.append(loss)
+		loss = sum(history.history['loss'])
+		losses.append(loss)
 
-	# validation
-	TP = TN = FP1 = FP2 = FN = 0
-	test_iter = iter(data_generator(BATCH_SIZE, x_test, y_test, FRAME_STACK))
-	test_steps = check_steps(x_test, BATCH_SIZE, FRAME_STACK)
-	logger.info("==========Epoch {} start validation==========".format(i))
-	for j in range(test_steps):
-		x_batch, y_batch = next(test_iter)
-		y_pred = model.predict(x_batch, batch_size=BATCH_SIZE)
-		y_pred = y_pred > 0.5
-		y_pred = y_pred.astype('float32')
+		# validation
+		TP = TN = FP1 = FP2 = FN = 0
+		test_iter = iter(data_generator(BATCH_SIZE, x_test, y_test, FRAME_STACK))
+		test_steps = check_steps(x_test, BATCH_SIZE, FRAME_STACK)
+		logger.info("==========Epoch {} start validation==========".format(i))
+		for j in range(test_steps):
+			x_batch, y_batch = next(test_iter)
+			y_pred = model.predict(x_batch, batch_size=BATCH_SIZE)
+			y_pred = y_pred > 0.5
+			y_pred = y_pred.astype('float32')
 
-		tp, tn, fp1, fp2, fn = confusion(y_pred, y_batch[:, 0,...], tol)
-		TP += tp
-		TN += tn
-		FP1 += fp1
-		FP2 += fp2
-		FN += fn
+			tp, tn, fp1, fp2, fn = confusion(y_pred, y_batch[:, 0,...], tol)
+			TP += tp
+			TN += tn
+			FP1 += fp1
+			FP2 += fp2
+			FN += fn
 
-	accuracy, precision, recall = compute_acc((TP, TN, FP1, FP2, FN))
-	avg_acc = (accuracy + precision + recall)/3
-	logger.info("Epoch {} accuracy: {:.3f}".format(i, accuracy))
-	logger.info("Epoch {} precision: {:.3f}".format(i, precision))
-	logger.info("Epoch {} recall: {:.3f}".format(i, recall))
-	logger.info("Epoch {} average = (accuracy + precision + recall)/3: {:.3f}".format(i, avg_acc))
+		accuracy, precision, recall = compute_acc((TP, TN, FP1, FP2, FN))
+		avg_acc = (accuracy + precision + recall)/3
+		logger.info("Epoch {} accuracy: {:.3f}".format(i, accuracy))
+		logger.info("Epoch {} precision: {:.3f}".format(i, precision))
+		logger.info("Epoch {} recall: {:.3f}".format(i, recall))
+		logger.info("Epoch {} average = (accuracy + precision + recall)/3: {:.3f}".format(i, avg_acc))
 
-	# learnging rate callback and saving model
-	if loss < best_loss - min_delta:
-		wait = 0
-		best_loss = loss
-		model.save_weights(save_weights)
-	else:
-		wait += 1
-		if wait >= patience_factor:
-			old_lr = float(K.get_value(model.optimizer.lr))
-			if old_lr > min_lr:
-				new_lr = old_lr * lr_factor
-				new_lr = max(new_lr, min_lr)
-				K.set_value(model.optimizer.lr, new_lr)
-				wait = 0
-				logger.info("Reduce model learning rate to {}".format(new_lr))
+		# learnging rate callback and saving model
+		if loss < best_loss - min_delta:
+			wait = 0
+			best_loss = loss
+			model.save_weights(save_weights)
+		else:
+			wait += 1
+			if wait >= patience_factor:
+				old_lr = float(K.get_value(model.optimizer.lr))
+				if old_lr > min_lr:
+					new_lr = old_lr * lr_factor
+					new_lr = max(new_lr, min_lr)
+					K.set_value(model.optimizer.lr, new_lr)
+					wait = 0
+					logger.info("Reduce model learning rate to {}".format(new_lr))
 
 plt.plot(losses)
 plt.title('Model loss')
